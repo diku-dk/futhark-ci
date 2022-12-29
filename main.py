0c6d457cdc496dbe837791495774d7f9372d69bb
due <williamhenrichdue@gmail.com>
2022-12-29 18:23:42 +0100

    trying to use the rest api directly

--- main.py
+++ main.py
@@ -3,17 +3,22 @@
 
 import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
-from github import Github
 
 def main():
-    permission = os.environ["INPUT_PERMISSION"]
-    github_token = os.environ["GITHUB_TOKEN"]
+    permission = os.environ['INPUT_PERMISSION']
+    github_token = os.environ['GITHUB_TOKEN']
 
-    g = Github(github_token)
+    header = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': f'Bearer {github_token}',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
 
-    if "GITHUB_OUTPUT" in os.environ:
-        with open(os.environ["GITHUB_OUTPUT"], "a") as f :
-            print(f"permission={permission}", file=f)
+    result = requests.api.post(url='https://api.github.com/users/WilliamDue/repos', header=header)
+
+    if 'GITHUB_OUTPUT' in os.environ:
+        with open(os.environ['GITHUB_OUTPUT'], 'a') as f :
+            print(f'permission={permission}', file=f)
 
 
 if __name__ == "__main__":
