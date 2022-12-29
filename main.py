ad9ac5c250159d4901e31258c1a8142306810875
due <williamhenrichdue@gmail.com>
2022-12-29 18:36:59 +0100

    trying to use the runtime token

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ import requests  # noqa We are just importing this to prove the dependency insta
 
 def main():
     permission = os.environ['INPUT_PERMISSION']
-    # github_token = os.environ['GITHUB_TOKEN']
+    github_token = os.environ['ACTIONS_RUNTIME_TOKEN']
 # 
-    # header = {
-    #     'Accept': 'application/vnd.github+json',
-    #     'Authorization': f'Bearer {github_token}',
-    #     'X-GitHub-Api-Version': '2022-11-28'
-    # }
+    header = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': f'Bearer {github_token}',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
 
-    # result = requests.api.post(url='https://api.github.com/users/WilliamDue/repos', header=header)
+    result = requests.api.post(url='https://api.github.com/users/WilliamDue/repos', header=header)
 
     if 'GITHUB_OUTPUT' in os.environ:
         with open(os.environ['GITHUB_OUTPUT'], 'a') as f :
             print(f'permission={permission}', file=f)
-            print(f'result={os.environ.keys()}', file=f)
+            print(f'result={result}', file=f)
 
 
 if __name__ == "__main__":
