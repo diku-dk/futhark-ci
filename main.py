59405837bd05d461ce408680005f8de8a70278ef
due <williamhenrichdue@gmail.com>
2022-12-29 18:01:52 +0100

    test

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import requests  # noqa We are just importing this to prove the dependency insta
 from github import Github
 
 def main():
-    permission = os.environ["INPUT_MYINPUT"]
+    permission = os.environ["INPUT_PERMISSION"]
     github_token = os.environ["GITHUB_TOKEN"]
 
     g = Github(github_token)
 
     if "GITHUB_OUTPUT" in os.environ:
         with open(os.environ["GITHUB_OUTPUT"], "a") as f :
-            print(f"myOutput={permission}", file=f)
+            print(f"permission={permission}", file=f)
 
 
 if __name__ == "__main__":
