9eb6b8c5ab7b4a0fcad7c6c2c5d956c5c182d748
due <williamhenrichdue@gmail.com>
2022-12-29 16:58:26 +0100

    Local Test of Hello World

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# Copyright (C) 2018 GitHub, Inc. and contributors
+# Copyright (C) 2018  Jacob Tomlinson
+
 import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
-
+import yamllint
 
 def main():
     my_input = os.environ["INPUT_MYINPUT"]
