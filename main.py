261e31ec2bd443052bfe46088ce5d8dc282f1690
due <williamhenrichdue@gmail.com>
2022-12-29 17:42:59 +0100

    Something went wrong

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import requests  # noqa We are just importing this to prove the dependency insta
 from github import Github
 
 def main():
-    permission = os.environ["INPUT_PERMISSION"]
+    permission = os.environ["INPUT_MYINPUT"]
     github_token = os.environ["GITHUB_TOKEN"]
 
     g = Github(github_token)
 
     if "GITHUB_OUTPUT" in os.environ:
         with open(os.environ["GITHUB_OUTPUT"], "a") as f :
-            print(f"permission={permission}", file=f)
+            print(f"myOutput={permission}", file=f)
 
 
 if __name__ == "__main__":
