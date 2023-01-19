e067bb43a1dec45b1a0016c52fa056d8a3f239d3
due <williamhenrichdue@gmail.com>
2023-01-20 00:00:21 +0100

    Made some of the changes discussed during last meeting

--- slurmbench.py
+++ slurmbench.py
@@ -1,5 +1,6 @@
 import optparse
 import os
+import stat
 from typing import Union
 
 
@@ -22,7 +23,7 @@ def is_any_none_flags(flags: dict[str, Union[str, None]]) -> Union[Exception, No
     Parameters
     ----------
     flags : dict[str, Union[str, None]]
-        The flags which have been passed to the installation script.
+        The flags which have been passed to the script.
     
     Returns
     -------
@@ -40,14 +41,51 @@ def is_any_none_flags(flags: dict[str, Union[str, None]]) -> Union[Exception, No
 
 
 def format_json_flag(flags: dict[str, None]) -> dict[str, None]:
+    '''
+    Given the dictionary of flags it will take the json flags and format it into the 
+    futhark-options flag.
+    Parameters
+    ----------
+    flags : dict[str, Union[str, None]]
+        The flags which have been passed to the script.
+    
+    Returns
+    -------
+    dict[str, None]
+        The new formatted flags.
+    '''
+
     flags = flags.copy()
-    json = flags['json']
+    json = flags.get('json')
+    if json is None:
+        raise Exception('The json flags must be set.')
     flags['futhark-options'] = f'--json {json} ' + flags['futhark-options']
     flags['futhark-options'] = flags['futhark-options'].strip()
     return flags
 
 
 def format_gpu_flag(flags: dict[str, None]) -> dict[str, None]:
+    '''
+    Given the dictionary of flags it will take the json flags and format it into the 
+    slurm-options flag. 
+    
+    Examples:
+    --gpu=somegpu becomes --gres=gpu:somegpu:1
+    --gpu=somegpu:2 becomes --gres=gpu:somegpu:2
+    --gpu=gpu becomes --gres=gpu:1
+    --gpu=gpu:3 becomes --gres=gpu:3
+    
+    Parameters
+    ----------
+    flags : dict[str, None]
+        The flags which have been passed to the script.
+    
+    Returns
+    -------
+    dict[Exception, None]
+        The new formatted flags.
+    '''
+
     flags = flags.copy()
 
     if flags.get('gpu') is None:
@@ -84,13 +122,32 @@ def format_gpu_flag(flags: dict[str, None]) -> dict[str, None]:
 
 
 def get_flags() -> dict[str, str]:
+    '''
+    Retrives the flags passed to the script and checks them for their validity and returns them.
+    The flags return should be a dictionary with only four keys. Do not set --gres or -p manually
+    for slurm and dot not set --json manually for futhark.
+
+    Example:
+    {
+        'futhark': '/path/to/futhark.tar.xv',
+        'benchmarks': '/path/to/benchmarks/',
+        'futhark-options': --some --valid --futhark --options,
+        'slurm-options': --some --valid --slurm --options
+    }
+    
+    Returns
+    -------
+    dict[str, str]
+        The new formatted flags.
+    '''
 
     parser = optparse.OptionParser()
     parser.add_option('-g', '--gpu', dest='gpu', type='string', metavar='GPU:AMOUNT',
-help='''the name of the GPU to use found here https://diku-dk.github.io/wiki/slurm-cluster
-and the specified AMOUNT of gpus formatted as GPU:AMOUNT.''')
+                      help=('the name of the GPU to use found here '
+                            'https://diku-dk.github.io/wiki/slurm-cluster '
+                            'and the specified AMOUNT of gpus formatted as GPU:AMOUNT.'))
     parser.add_option('-f', '--futhark', dest='futhark', type='string', metavar='FUTHARK',
-                      help='path to the binaries of the FUTHARK compiler.')
+                      help='path to tar file with the binaries of the FUTHARK.')
     parser.add_option('-b', '--benchmarks', dest='benchmarks', type='string', metavar='BENCHMARKS',
                       help='path to the BENCHMARKS.')
     parser.add_option('--fo', '--futhark-options', dest='futhark-options', type='string', default='',
@@ -122,18 +179,46 @@ def main() -> None:
     benchmarks = flags['benchmarks']
     futhark_options = flags['futhark-options']
     slurm_options = flags['slurm-options']
+
+    cwd = os.getcwd()
+    print(f'Current Working Directory: {cwd}')
+
+    futhark_folder = 'futhark-nightly'
+    if not os.path.exist(futhark_folder):
+        os.mkdir(futhark_folder)
+
+    print('Extracting Futhark Binaries:')
+    if os.system(f'tar xvf {futhark} -C {futhark_folder} --strip-components 1') != 0:
+        raise Exception(f'Something went wrong during extraction of {futhark}')
     
-    with open('temp.sh', mode='w') as fp:
+    futhark_bin = os.path.join(futhark_folder, 'bin', 'futhark')
+
+    get_data_script = os.path.join('.', benchmarks, "get-data.sh")
+
+    if not os.path.exists(get_data_script):
+        raise Exception(f'The script "{get_data_script}" does not exist.')
+
+    print('Getting Benchmark Data:')
+    get_data_command = f'{get_data_script} external-data.txt'
+    if os.system(get_data_command) != 0:
+        raise Exception(f'Something went wrong during "{get_data_command}".')
+
+    print('Enviroment Variables:')
+    for var, val in os.environ:
+        print(f'{var}={val}')
+
+    script = 'temp.sh'
+    with open(script, mode='w') as fp:
         fp.write('#!/bin/bash\n')
-        fp.write(f'{futhark} bench {benchmarks} {futhark_options}')
+        fp.write(f'{futhark_bin} bench {benchmarks} {futhark_options}')
     
-    # for some reason os.chmod does not work.
-    os.system('chmod +x temp.sh')
+    os.chmod(script, os.stat(script).st_mode | stat.S_IEXEC)
     
+    print('Running Benchmark:')
     if os.system(f'srun {slurm_options} temp.sh') != 0:
         raise Exception('Something went wrong during srun.')
         
-    os.remove('temp.sh')
+    os.remove(script)
 
 if __name__ == '__main__':
     main()
